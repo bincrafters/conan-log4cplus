954d703757b1c1aa70891bbf72a2044c8fff6099
SSE4 <tomskside@gmail.com>
2018-02-14 11:33:00 +0700

    - use CMake install

--- conanfile.py
+++ conanfile.py
@@ -31,22 +31,10 @@ class Log4cplusConan(ConanFile):
         cmake = CMake(self)
         cmake.configure(build_dir=self.build_subfolder)
         cmake.build()
+        cmake.install()
 
     def package(self):
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
-        build_dir = os.path.join(self.build_subfolder, self.source_subfolder)
-        build_dir_include = os.path.join(build_dir, "include")
-
         self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*", dst="include", src=build_dir_include)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
