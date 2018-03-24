a1d2eece36dac9ba1d64f4f5d15b706734a81e76
Jerry W <jerrywiltse@gmail.com>
2018-03-24 13:47:31 -0400

    merge 1.2.1 conan-center changes to 2.0.0-rc2, despite being some differences

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanException
 import os
 
 
@@ -17,28 +18,66 @@ class Log4cplusConan(ConanFile):
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
     settings = 'os', 'compiler', 'build_type', 'arch'
-    options = {'shared': [True, False], "fPIC": [True, False]}
-    default_options = 'shared=False', 'fPIC=True'
+    options = {'shared': [True, False],
+               "fPIC": [True, False],
+               "single_threaded": [True, False],
+               "build_logging_server": [True, False],
+               "with_iconv": [True, False],
+               "working_locale": [True, False],
+               "working_c_locale": [True, False],
+               "decorated_name": [True, False],
+               "qt4_debug_appender": [True, False],
+               "qt5_debug_appender": [True, False]}
+    default_options = ('shared=False',
+                       'fPIC=True',
+                       "single_threaded=False",
+                       "build_logging_server=False",
+                       "with_iconv=False",
+                       "working_locale=False",
+                       "working_c_locale=False",
+                       "decorated_name=False",
+                       "qt4_debug_appender=False",
+                       "qt5_debug_appender=False")
     short_paths = True
 
-    def config(self):
+    def requirements(self):
+        if self.options.with_iconv:
+            self.requires.add('libiconv/1.15@bincrafters/stable')
+
+    def config_options(self):
         if self.settings.compiler == 'Visual Studio':
             del self.options.fPIC
+
+    def configure(self):
+        if self.options.qt4_debug_appender:
+            raise ConanException('Qt4 debug appender is not supported yet!')
+        if self.options.qt5_debug_appender:
+            raise ConanException('Qt5 debug appender is not supported yet!')
 
     def source(self):
         source_url = "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable"
         archive_name = self.name + "-" + self.version
-        tools.get("{0}/2.0.0/{1}.zip".format(source_url, archive_name))
+        version_trimmed = self.version.replace("-rc2","")
+        tools.get("{0}/{1}/{2}.zip".format(source_url, version_trimmed, archive_name))
         os.rename(archive_name, self.source_subfolder)
 
     def build(self):
         cmake = CMake(self)
         if self.settings.compiler != 'Visual Studio':
             cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
+
+        cmake.definitions['LOG4CPLUS_BUILD_TESTING'] = False
+        cmake.definitions['WITH_UNIT_TESTS'] = False
+        cmake.definitions["LOG4CPLUS_ENABLE_DECORATED_LIBRARY_NAME"] = self.options.decorated_name
+        cmake.definitions['LOG4CPLUS_QT4'] = self.options.qt4_debug_appender
+        cmake.definitions['LOG4CPLUS_QT5'] = self.options.qt5_debug_appender
+        cmake.definitions['LOG4CPLUS_SINGLE_THREADED'] = self.options.single_threaded
+        cmake.definitions['LOG4CPLUS_BUILD_LOGGINGSERVER'] = self.options.build_logging_server
+        cmake.definitions['WITH_ICONV'] = self.options.with_iconv
+        cmake.definitions['LOG4CPLUS_WORKING_LOCALE'] = self.options.working_locale
+        cmake.definitions['LOG4CPLUS_WORKING_C_LOCALE'] = self.options.working_c_locale
+
         cmake.configure(build_dir=self.build_subfolder)
-        cmake.definitions['LOG4CPLUS_BUILD_TESTING'] = 'False'
-        cmake.definitions['WITH_UNIT_TESTS'] = 'False'
-        cmake.definitions["LOG4CPLUS_ENABLE_DECORATED_LIBRARY_NAME"] = 'False'
         cmake.build()
         cmake.install()
 
