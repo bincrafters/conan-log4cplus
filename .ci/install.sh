caa55922e4ea79fbdfd69b2cffa8073e6832bfbb
Uilian Ries <uilianries@gmail.com>
2019-01-17 11:34:27 -0200

    Add ci folder
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

---
+++ .ci/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    brew update || brew update
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    brew install cmake || true
+    brew install nasm || true
+
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
+    pyenv rehash
+    pyenv activate conan
+else
+    sudo apt-get update 
+	sudo apt-get install -y nasm autoconf dh-autoreconf
+fi
+
+pip install conan_package_tools  bincrafters_package_tools
+conan user
