78d64d3e82774aface8bb1050edeac6e932d5ff6
Uilian Ries <uilianries@gmail.com>
2019-01-21 20:47:32 -0200

    Fix pyOpenSSL minimal version on OSX
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/install.sh
+++ .ci/install.sh
@@ -14,12 +14,12 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 3.7.1
-    pyenv virtualenv 3.7.1 conan
+    pyenv install 2.7.10
+    pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
 else
-    sudo apt-get update 
+    sudo apt-get update
 	sudo apt-get install -y nasm autoconf dh-autoreconf
 fi
 
